1168d615120eec45ee0dc50a6af4ba24a93ee96f
Kevin <kevin@kokev.de>
2025-06-28 19:01:43 +0200

    Add inlay hints

--- src/server/vrl.rs
+++ src/server/vrl.rs
@@ -1,16 +1,23 @@
+use anyhow::anyhow;
 use language_server::{
     cache::Cache,
     diagnostics::Diagnostics,
     server::{LSPConnection, LSPError, LSPServer},
 };
 use lsp_types::{
-    CompletionList, CompletionResponse, Diagnostic, ServerCapabilities, TextDocumentSyncKind,
-    TextDocumentSyncOptions,
+    CompletionList, CompletionResponse, Diagnostic, InlayHint, InlayHintLabel, OneOf, Position,
+    ServerCapabilities, TextDocumentSyncKind, TextDocumentSyncOptions,
 };
+use ropey::Rope;
+use tree_sitter::Node;
 
 use crate::{
     ast::VrlAstGenerator,
-    completion::{Completion, global::GlobalCompletion, std::StdCompletion},
+    completion::{
+        Completion,
+        global::GlobalCompletion,
+        std::{StdCompletion, StdFunction, StdFunctions},
+    },
     diagnostics::compile::CompileDiagnostics,
 };
 
@@ -47,7 +54,7 @@ impl LSPServer for VRLServer {
             completion_provider: Some(lsp_types::CompletionOptions {
                 ..Default::default()
             }),
-
+            inlay_hint_provider: Some(OneOf::Left(true)),
             ..Default::default()
         }
     }
@@ -73,4 +80,102 @@ impl LSPServer for VRLServer {
         };
         Ok(CompletionResponse::List(completion_list).into())
     }
+
+    fn inlay_hint(
+        &self,
+        params: <lsp_types::request::InlayHintRequest as lsp_types::request::Request>::Params,
+    ) -> anyhow::Result<language_server::server::LSPResponse, LSPError> {
+        let doc = self.cache.get_document(params.text_document.uri.as_str())?;
+
+        let tree = doc
+            .get_ast()?
+            .tree
+            .clone()
+            .ok_or(anyhow!("AST was never parsed"))?;
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&tree.language())
+            .or_else(|e| Err(anyhow!("Setting language of parser: {e}")))?;
+        let query = tree_sitter::Query::new(&tree.language(), "(function_call (arguments) @call)")
+            .expect("BUG: Invalid query");
+        let mut cursor = tree_sitter::QueryCursor::new();
+        let captures = cursor.captures(&query, tree.root_node(), doc.content.as_bytes());
+        let rope = Rope::from_str(&doc.content);
+        let std_functions = StdFunctions::generate();
+        let items: Vec<InlayHint> = captures
+            .flat_map(|(q_match, _)| {
+                q_match.captures.iter().flat_map(|capture| {
+                    let Some(std_func) = get_std_function(capture.node, &rope, &std_functions)
+                    else {
+                        return vec![];
+                    };
+                    let Some(arg_nodes) = get_argument_nodes(capture.node) else {
+                        return vec![];
+                    };
+
+                    arg_nodes
+                        .iter()
+                        .enumerate()
+                        .filter_map(|(i, node)| {
+                            Some(InlayHint {
+                                label: InlayHintLabel::String(format!(
+                                    "{}:",
+                                    std_func.arguments.get(i)?.name
+                                )),
+                                position: Position {
+                                    line: node.start_position().row as u32,
+                                    character: node.start_position().column as u32,
+                                },
+                                kind: None,
+                                text_edits: None,
+                                tooltip: None,
+                                padding_right: Some(true),
+                                padding_left: None,
+                                data: None,
+                            })
+                        })
+                        .collect::<Vec<InlayHint>>()
+                })
+            })
+            .collect();
+
+        Ok(items.into())
+    }
+}
+
+fn get_std_function<'a>(
+    call_node: Node,
+    rope: &'a Rope,
+    std_functions: &'a StdFunctions,
+) -> Option<&'a StdFunction> {
+    let mut prev_sibling = call_node.prev_sibling()?;
+    if prev_sibling.grammar_name() == "!" {
+        prev_sibling = prev_sibling.prev_sibling()?;
+    }
+    let function_name = get_node_name(&prev_sibling, &rope);
+    Some(std_functions.functions.get(&function_name.to_string())?)
+}
+
+fn get_argument_nodes(call_node: Node) -> Option<Vec<Node>> {
+    let mut arg_nodes = vec![];
+    let mut arg_cursor = call_node.walk();
+    // This is (
+    arg_cursor.goto_first_child();
+    while arg_cursor.goto_next_sibling() && arg_cursor.node().grammar_name() != ")" {
+        if arg_cursor.node().grammar_name() != "," {
+            arg_nodes.push(arg_cursor.node());
+        }
+    }
+
+    Some(arg_nodes)
+}
+
+fn get_node_name<'a>(node: &'a Node, rope: &'a Rope) -> &'a str {
+    let start = node.start_position();
+    let end = node.end_position();
+    let start_char = rope.line_to_char(start.row) + start.column;
+    let end_char = rope.line_to_char(end.row) + end.column;
+    rope.slice(start_char..end_char)
+        .as_str()
+        .unwrap_or_default()
 }
